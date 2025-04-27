4036b338bed30dac8010bd2e86bc1ae7bff1caf2
printfCYQ <459987870@qq.com>
2025-04-27 23:08:31 +0800

    layout tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-[#E1E2E3]"
+    >
+      <Text className="text-light-100 text-2xl font-bold underline">Welcome react native</Text>
+    </View>
+  );
+}
---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import logo from "@/assets/icons/logo.png";
+import star from "@/assets/icons/star.png";
+import play from "@/assets/icons/play.png";
+import arrow from "@/assets/icons/arrow.png";
+
+export const icons = {
+  logo,
+  star,
+  play,
+  arrow,
+};
